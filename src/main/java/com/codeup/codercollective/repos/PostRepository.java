48c2ddb5b9ee35d8707c465344ebd96ba5c4bcc4
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-09-21 14:35:00 -0500

    fixed forum controller. Now are able to see post details for a certain forum

--- src/main/java/com/codeup/codercollective/repos/PostRepository.java
+++ src/main/java/com/codeup/codercollective/repos/PostRepository.java
@@ -1,14 +1,10 @@
 package com.codeup.codercollective.repos;
 
-import com.codeup.codercollective.model.Forum;
 import com.codeup.codercollective.model.Post;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 
-    List<Post> findByForums(Forum id);
 
-    Iterable<Post> findAll();
 }
