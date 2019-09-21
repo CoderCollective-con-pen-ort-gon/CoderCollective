2bcabe9a6f80ddea51d00daa6848daff393753e5
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-09-21 16:30:42 -0500

    added pictures and changed date time to database

--- src/main/java/com/codeup/codercollective/controllers/UserController.java
+++ src/main/java/com/codeup/codercollective/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.codeup.codercollective.controllers;
 
+import com.codeup.codercollective.model.Forum;
 import com.codeup.codercollective.model.User;
+import com.codeup.codercollective.repos.ForumRepository;
 import com.codeup.codercollective.repos.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +13,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class UserController {
     private final UserRepository userDao;
+    private final ForumRepository forumDao;
 
-    public UserController(UserRepository userRepository){
+    public UserController(UserRepository userRepository,ForumRepository forumRepository){
         userDao = userRepository;
+        forumDao=forumRepository;
     }
 
     @GetMapping("/profile/{id}")
     public String getUserProfile(@PathVariable long id, Model vModel){
         User user=userDao.findOne(id);
+        Iterable<Forum> forums = forumDao.findAll();
+        vModel.addAttribute("forums", forums);
         vModel.addAttribute("user",user);
 
         return"users/profile";
