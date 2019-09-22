164e9f517def1778c6155671c5a26806fd832945
victorpena13 <victor.pena1392@gmail.com>
2019-09-21 21:08:28 -0500

    registered users

--- src/main/java/com/codeup/codercollective/controllers/UserController.java
+++ src/main/java/com/codeup/codercollective/controllers/UserController.java
@@ -4,20 +4,25 @@ import com.codeup.codercollective.model.Forum;
 import com.codeup.codercollective.model.User;
 import com.codeup.codercollective.repos.ForumRepository;
 import com.codeup.codercollective.repos.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class UserController {
     private final UserRepository userDao;
     private final ForumRepository forumDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepository,ForumRepository forumRepository){
+    public UserController(UserRepository userRepository,ForumRepository forumRepository, PasswordEncoder passwordEncoder){
         userDao = userRepository;
-        forumDao=forumRepository;
+        forumDao = forumRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/profile/{id}")
@@ -29,6 +34,22 @@ public class UserController {
 
         return"users/profile";
     }
+
+    @GetMapping("/register")
+    public String signUp(Model vModel){
+        vModel.addAttribute("user", new User());
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
+
+
 
 
 
