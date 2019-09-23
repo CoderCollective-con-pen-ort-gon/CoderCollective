d5798f2bd4d852ae89e6aa1694d5190d2796209f
victorpena13 <victor.pena1392@gmail.com>
2019-09-23 18:10:00 -0500

    user can view their individual posts on profile

--- src/main/java/com/codeup/codercollective/repos/PostRepository.java
+++ src/main/java/com/codeup/codercollective/repos/PostRepository.java
@@ -1,10 +1,11 @@
 package com.codeup.codercollective.repos;
 
 import com.codeup.codercollective.model.Post;
+import com.codeup.codercollective.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 
-
+    Iterable<Post> findByOwner(User user);
 }
