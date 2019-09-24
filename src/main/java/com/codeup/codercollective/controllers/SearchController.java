23d939ea5d296208ce9c6fcbc1c44dab83976c40
JustinSOrtega <justinsortega@gmail.com>
2019-09-24 12:35:39 -0500

    search done nowmmaking it oretty and connecting to navbar

--- src/main/java/com/codeup/codercollective/controllers/SearchController.java
+++ src/main/java/com/codeup/codercollective/controllers/SearchController.java
@@ -1,42 +1,45 @@
-//package com.codeup.codercollective.controllers;
-//
-//import com.codeup.codercollective.model.Forum;
-//import com.codeup.codercollective.model.Post;
-//import com.codeup.codercollective.repos.ForumRepository;
-//import com.codeup.codercollective.repos.PostRepository;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//import java.util.List;
-//
-//@Controller
-//public class SearchController {
-//
-//    private final PostRepository postDao;
-//    private final ForumRepository forumDao;
-//
-//    public SearchController(PostRepository postRepository, ForumRepository forumRepository) {
-//        postDao = postRepository;
-//        forumDao = forumRepository;
-//    }
-//
-//
-//    @GetMapping("/forums")
-//    public String forum(Model vModel){
-//        Iterable<Forum> forums = forumDao.findAll();
-//        vModel.addAttribute("forums", forums);
-//        return "posts/forums";
-//    }
-//
-//    @GetMapping("/forums/{id}")
-//    public String show(@PathVariable long id, Model vModel) {
-//        Forum forumId = forumDao.findOne(id);
-//        List <Post> posts=forumId.getPosts();
-//        vModel.addAttribute("forum", forumId);
-//        vModel.addAttribute("posts", posts);
-//        return "posts/posts";
-//    }
-//
-//}
+package com.codeup.codercollective.controllers;
+
+import com.codeup.codercollective.model.Post;
+import com.codeup.codercollective.model.User;
+import com.codeup.codercollective.repos.PostRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class SearchController {
+
+    private final PostRepository postDao;
+
+    public SearchController(PostRepository postRepository) {
+        postDao = postRepository;
+    }
+
+
+    @GetMapping("/search")
+    public String showPosts(Model vModel){
+//        vModel.addAttribute("searched", null);
+//        vModel.addAttribute("posts", null);
+        return "posts/search";
+    }
+
+    @PostMapping("/search")
+    public String show(@RequestParam(value = "searched", required = false) String searched, Model vModel) {
+        User loggedIn = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("UserId = " + loggedIn.getId());
+        List<Post> postsSearched;
+        postsSearched = postDao.findAllByBodyOrTitle(searched);
+
+        vModel.addAttribute("posts", postsSearched);
+        vModel.addAttribute("searched", searched);
+        return "posts/search";
+    }
+
+}
--- src/main/java/com/codeup/codercollective/repos/ForumRepository.java
+++ src/main/java/com/codeup/codercollective/repos/ForumRepository.java
@@ -1,9 +1,10 @@
 package com.codeup.codercollective.repos;
 
 import com.codeup.codercollective.model.Forum;
+import com.codeup.codercollective.model.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface ForumRepository extends CrudRepository<Forum, Long> {
-
 }
