e07d83d2e05f2e399d3be6a19a72b60a2a49cb08
victorpena13 <victor.pena1392@gmail.com>
2019-09-25 16:55:20 -0500

    fixed the view for logged out users

--- src/main/java/com/codeup/codercollective/controllers/PostController.java
+++ src/main/java/com/codeup/codercollective/controllers/PostController.java
@@ -33,8 +33,8 @@ public class PostController {
 
     @GetMapping("/posts/{id}")
     public String show(@PathVariable long id, Model vModel) {
-        User loggedIn = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        System.out.println("UserId = " + loggedIn.getId());
+//        User loggedIn = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        System.out.println("UserId = " + loggedIn.getId());
 
         Post postId = postDao.findOne(id);
         Iterable<Comment> comments = postId.getComments();
