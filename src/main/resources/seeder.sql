37766eab3cf7388565a103d52aec154db9f0a38c
victorpena13 <victor.pena1392@gmail.com>
2019-09-25 12:22:16 -0500

    updated seeder

--- src/main/resources/seeder.sql
+++ src/main/resources/seeder.sql
@@ -1,44 +1,46 @@
-use codercollective_db;
-
+use cc_db;
+truncate users;
 insert into users(id, email, firstname, lastname, password, title, username) values
-(1, 'alexchris@gmail.com', 'alex', 'casa', 'pass', 'senior software developer', 'alex'),
-(2, 'victorpena@gmail.com', 'victor', 'pena', 'pass', 'junior software developer', 'victor'),
-(3, 'joseph@gmail.com', 'joesph', 'ortega', 'pass', 'senior software developer', 'joseph');
-
+(1, 'alexchris99@gmail.com', 'alex', 'contreras', 'password', 'Java Developer', 'alex'),
+(2, 'victorpena@gmail.com', 'victor', 'pena', 'password', 'Front-End Software Developer', 'victor'),
+(3, 'joseph@gmail.com', 'joesph', 'ortega', 'password', 'Full-Stack Software Developer', 'joseph'),
+(3, 'joey@gmail.com', 'Joey', 'Gonzalez', 'password', 'Full-Stack Software Developer', 'joey');
+truncate forums;
 insert into forums(id, name) values
-(1, 'springboot'),
-(2, 'javascript'),
-(3, 'python');
-
+(1, 'Ruby'),
+(2, 'JavaScript'),
+(3, 'Python'),
+(4,'Java'),
+(5,'C++'),
+(6,'Kotlin'),
+(7,'Elixir'),
+(8,'Go'),
+(9,'PHP'),
+(10,'Scala'),
+(11,'Clojure'),
+(12,'Frameworks'),
+(13,'CSS Frameworks'),
+(14,'Libraries'),
+(15,'IDE'),
+(16,'Learn Programming'),
+(17,'Tech Events');
+truncate posts;
 insert into posts(id, body, title, user_id, create_date,forum_id) values
-(1, 'I need help with springboot', 'HELP', 1, '2010-10-10',1),
-(2, 'Javascript tutorial', 'helloworld', 1, '2009-10-10',2);
-
-
-
-insert into posts(id, body, title, user_id, create_date,forum_id) values
-(1, 'I need help with springboot', 'HELP', 1, '2010-10-10',1),
+(1, 'I need help with springboot', 'HELP', 1, '2010-10-10',12),
 (2, 'Javascript tutorial', 'helloworld', 2, '2009-10-10',2);
-
+truncate comments;
 insert into comments(id, body, post_id, user_id) values
 (1, 'you saved my life', 2, 1),
 (2, 'thanks for the sprinboot tip', 1, 1);
-
+truncate favorite_post;
 insert into favorite_post(user_id, post_id) values
 (1,2),
 (2,1),
 (2,2);
-
+truncate comments;
 insert into rating(id, vote, comment_id, owner_id) values
 (1, 1, 1, 2),
 (2, 0, 2, 1);
-
-
-
-
-
-
-
 
 
 
