4b1d7aa7d15567a1d010cb7d1260a4aed24064c1
JustinSOrtega <justinsortega@gmail.com>
2019-10-04 18:08:12 -0500

    EoD Friday trying to fix google issue

--- src/main/java/com/codeup/codercollective/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/codercollective/controllers/AuthenticationController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
-    public String showLoginform() {
+    public String showLoginform(Model vModel) {
+        vModel.addAttribute("alreadyExists", false);
         return "users/login";
     }
 }
